7983793af8d9629ec37fd4ce59d9e235b899c00c
AlexanderR <96749659+Nazeofel@users.noreply.github.com>
2022-07-24 00:28:45 +0200

    Update
    Update

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { invoke } from '@tauri-apps/api'
+import { invoke } from './@tauri-apps/api'
 import './App.scss'
 //import axios from 'axios';
 
